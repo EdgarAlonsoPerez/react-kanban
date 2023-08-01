5583f5029d0cc5f7807f7de1249b21885625d076
Logan <ingperezedgar@gmail.com>
2023-08-01 11:50:42 -0600

    feat(react): Tasks are now components controlled by useState, and the Next and Prev buttons function to move tasks through different statuses.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,68 @@
+import { useState } from 'react';
 import './App.css'
 
+const INITIAL_TASKS = [
+  {
+    id: 1,
+    name: 'Learn React',
+    description: 'Complete the Ultimate React Course',
+    status: 2
+  },
+  {
+    id: 2,
+    name: 'Learn NestJS',
+    description: 'Complete the ultimate NestJs course',
+    status: 3
+  },
+  {
+    id: 3,
+    name: 'Learn Next.js',
+    description: 'Complete the ultimate NextJs course',
+    status: 1
+  }
+];
+
+const Task = ({ id, name, status, description, next, prev}) => {
+  const handlePrev = () => { 
+    prev(id)
+  }
+
+  const handleNext = () => { 
+    next(id)
+  }
+
+  return (<div className='task' id={id}>
+    <div className='task-header'>
+      <h4> {name} </h4>
+    </div>
+
+    <div className='task-body'>
+      <p> {description} </p>
+    </div>
+
+    <div className='task-footer'>
+      { status > 1 ? <button onClick={handlePrev}> Prev </button> : ''}
+      { status < 3 ? <button onClick={handleNext}> Next </button> : '' }
+    </div>
+  </div>)
+}
+
 function App() {
+  const [tasks, setTasks] = useState(INITIAL_TASKS)
+
+  const prev = (id) => {
+    const newTasks = [...tasks];
+    const index = newTasks.findIndex( t => t.id == id)
+    newTasks[index].status -= 1;
+    setTasks(newTasks);
+  }
+
+  const next = (id) => {
+    const newTasks = [...tasks];
+    const index = newTasks.findIndex( t =>  t.id == id )
+    newTasks[index].status += 1;
+    setTasks(newTasks)
+  }
 
   return (
     <>
@@ -9,75 +71,58 @@ function App() {
   
       <div className='column'>
         <h4> Pending Tasks </h4>
-        
-        <div className='task'>
-            <div className='task-header'>
-              <h4> Task 1 </h4>
-            </div>
 
-            <div className='task-body'>
-              <p> description </p>
-            </div>
-
-            <div className='task-footer'>
-              <button> Next </button>
-            </div>
-        </div>
-
-          <div className='task'>
-            <div className='task-header'>
-              <h4> Task 4 </h4>
-            </div>
-
-            <div className='task-body'>
-              <p> description </p>
-            </div>
-
-            <div className='task-footer'>
-              <button> Next </button>
-            </div>
-            
-          </div>
+        {tasks.map(t => {
+         return t.status == 1 ? (
+            <Task
+              key={t.id}
+              id={t.id}
+              name={t.name}
+              description={t.description}
+              status={t.status}
+              next={next}
+              prev={prev}
+            ></Task>
+          ) : ''
+        })}
 
       </div>
       
       <div className='column'>
         <h4> In Progress Tasks </h4>
        
-          <div className='task'>
-            <div className='task-header'>
-              <h4> Task 2 </h4>
-            </div>
-
-            <div className='task-body'>
-              <p> description </p>
-            </div>
-
-            <div className='task-footer'>
-              <button> Prev </button>
-              <button> Next </button>
-            </div>
-          </div>
+          {tasks.map(t => {
+            return t.status == 2 ? (
+              <Task
+                key={t.id}
+                id={t.id}
+                name={t.name}
+                description={t.description}
+                status={t.status}
+                next={next}
+                prev={prev}
+              ></Task>
+            ) : ''
+          })}
 
       </div>
       
       <div className='column'>
         <h4> Closed Tasks </h4>
         
-          <div className='task'>
-            <div className='task-header'>
-              <h4> Task 3 </h4>
-            </div>
-
-            <div className='task-body'>
-              <p> description </p>
-            </div>
-
-            <div className='task-footer'>
-              <button> Prev </button>
-            </div>
-
-          </div>
+          {tasks.map(t => {
+            return t.status == 3 ? (
+              <Task
+                key={t.id}
+                id={t.id}
+                name={t.name}
+                description={t.description}
+                status={t.status}
+                next={next}
+                prev={prev}
+              ></Task>
+            ) : ''
+          })}
 
       </div>
 
@@ -87,3 +132,4 @@ function App() {
 }
 
 export default App
+
